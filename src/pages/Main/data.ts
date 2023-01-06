e5dd36fbd6a2eada7d166c3e6a94c1beae02ecfd
lfs <luciano2023silva@gmail.com>
2023-01-06 15:58:09 -0300

    feat: component Task, and finish the challenge

---
+++ src/pages/Main/data.ts
@@ -0,0 +1,34 @@
+import { v4 } from 'uuid';
+
+export const currentList = [
+  {
+    id: v4(),
+    description:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    isDone: false,
+  },
+  {
+    id: v4(),
+    description:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    isDone: false,
+  },
+  {
+    id: v4(),
+    description:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    isDone: false,
+  },
+  {
+    id: v4(),
+    description:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    isDone: true,
+  },
+  {
+    id: v4(),
+    description:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    isDone: true,
+  },
+];
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,92 @@
 import { PlusCircle } from 'phosphor-react';
-import { EmptyList } from '../../components/EmptyList';
-import { List } from '../../components/List';
+import { ChangeEvent, InvalidEvent, useState } from 'react';
+import { Task } from '../../components/Task';
 
 import styles from './styles.module.scss';
 
+import { currentList } from './data';
+import { EmptyList } from '../../components/EmptyList';
+import { v4 } from 'uuid';
+
+interface ITask {
+  id?: string;
+  description?: string;
+  isDone?: boolean;
+}
+
 export function Main() {
+  const [list, setList] = useState<ITask[]>(currentList);
+  const [newTask, setNewTask] = useState({
+    id: '',
+    description: '',
+    isDone: false,
+  });
+
+  const totalTasks = list.length;
+
+  const totalDoneTasks = list.filter((task) => {
+    return task.isDone === true;
+  });
+
+  function handleCreateNewTask(event: ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const addTask = {
+      id: v4(),
+      description: event.target.task.value,
+      isDone: false,
+    };
+
+    setList([...list, addTask]);
+
+    setNewTask({ id: '', description: '', isDone: false });
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+
+    setNewTask((task) => {
+      return {
+        ...newTask,
+        description: event.target.value,
+        id: v4(),
+      };
+    });
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Esse campo é obrigatorio!');
+  }
+
+  function changeChecked(taskId: string) {
+    const newTasksListChecked = list.map((task) => {
+      if (task.id === taskId) {
+        task.isDone = !task.isDone;
+      }
+      return task;
+    });
+    setList(newTasksListChecked);
+  }
+
+  function deleteTask(taskId: string) {
+    const tasksWithoutDeletedOne = list?.filter((task) => {
+      return task.id !== taskId;
+    });
+    setList(tasksWithoutDeletedOne);
+  }
+
   return (
-    <div className={styles.mainContainer}>
-      <form className={styles.formTodo}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
+    <main className={styles.mainContainer}>
+      <form onSubmit={handleCreateNewTask} className={styles.formTodo}>
+        <input
+          name="task"
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          value={newTask.description}
+          onChange={handleNewTaskChange}
+          onInvalid={handleNewTaskInvalid}
+          required
+        />
         <button type="submit">
           Criar <PlusCircle size={18} />
         </button>
@@ -17,15 +95,37 @@ export function Main() {
         <header className={styles.headerBox}>
           <div className={styles.createdTasks}>
             <strong>Tarefas criadas</strong>
-            <span>0</span>
+            <span>{totalTasks}</span>
           </div>
-          <div className={styles.donedTasks}>
-            <strong>Concluídas</strong>
-            <span>2 de 5</span>
-          </div>
+          {totalTasks === 0 ? (
+            <div className={styles.donedTasks}>
+              <strong>Concluídas</strong>
+              <span>0</span>
+            </div>
+          ) : (
+            <div className={styles.donedTasks}>
+              <strong>Concluídas</strong>
+              <span>
+                {totalDoneTasks.length} de {totalTasks}
+              </span>
+            </div>
+          )}
         </header>
-        <List />
+        {list.length === 0 ? (
+          <EmptyList />
+        ) : (
+          <div className={styles.listContainer}>
+            {list.map((task) => (
+              <Task
+                key={task.id}
+                onCheck={changeChecked}
+                onDelete={deleteTask}
+                {...task}
+              />
+            ))}
+          </div>
+        )}
       </div>
-    </div>
+    </main>
   );
 }
